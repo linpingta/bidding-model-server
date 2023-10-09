8f3ddf396e525671e60ac44b0a7c4cb5f486cc16
LPT <chut1987@gmail.com>
2023-10-09 21:59:59 +0800

    Add files via upload

--- handler/bid_handler.cpp
+++ handler/bid_handler.cpp
@@ -3,12 +3,85 @@
 
 #include "bid_handler.h"
 
-void BidHandler::doBidding(const BidReqData& bidReqData, BidRspData& bidRspData) {
-  auto logger = getLogger();
+void BidHandler::Init() {
+  index_manager_ = IndexManager::getInstance();
+  logger_ = getLogger();
 }
 
-bool BidHandler::targetingFilter(const BidReqData& bidReqData) {
-  return false;
+void BidHandler::Bid(const BidReqData& bidReqData, BidRspData& bidRspData) {
+  if (!reqFilter(bidReqData)) {
+    logger_->info("invalid req, filter");
+    return;
+  }
+
+  auto campaignMap = campaignFilter(bidReqData);
+  if (campaignMap.empty()) {
+    logger_->info("no campaign after campaign check");
+    return;
+  }
+
+  auto adMap = adFilter(bidReqData, campaignMap);
+  if (adMap.empty()) {
+    logger_->info("no ad after ad check");
+    return;
+  }
+}
+
+bool BidHandler::reqFilter(const BidReqData &bidReqData) {
+  auto campaignIndex = index_manager_->getIndex(kCampaignIndex);
+  if (campaignIndex == nullptr) {
+    return false;
+  }
+  index_manager_->releaseIndex(campaignIndex);
+
+  auto adIndex = index_manager_->getIndex(kAdIndex);
+  if (adIndex == nullptr) {
+    return false;
+  }
+  index_manager_->releaseIndex(adIndex);
+
+  // random filter if needed
+
+  return true;
+}
+
+std::unordered_map<int32_t, Campaign> BidHandler::campaignFilter(const BidReqData& bidReqData) {
+  std::unordered_map<int32_t, Campaign> filteredMap;
+
+  auto index = index_manager_->getIndex(kCampaignIndex);
+  auto campaignIndex = dynamic_cast<CampaignIndex*>(index);
+  auto campaignMap = campaignIndex->getCampaignMap();
+
+  for (const auto& item: campaignMap) {
+    // filter campaign by req
+    // TODO
+
+    filteredMap[item.first] = item.second;
+  }
+
+  return filteredMap;
+}
+
+std::unordered_map<int32_t, Ad> BidHandler::adFilter(const BidReqData& bidReqData, const std::unordered_map<int32_t, Campaign>& campaignMap) {
+  std::unordered_map<int32_t, Ad> filteredMap;
+
+  auto index = index_manager_->getIndex(kAdIndex);
+  auto adIndex = dynamic_cast<AdIndex*>(index);
+  auto adMap = adIndex->getAdMap();
+
+  for (const auto& item: adMap) {
+    // filter ad by campaign
+    if (campaignMap.find(item.second.campaign_id) == campaignMap.end()) {
+      continue;
+    }
+
+    // filter ad
+    // TODO
+
+    filteredMap[item.first] = item.second;
+  }
+
+  return filteredMap;
 }
 
 bool BidHandler::rank() {
--- handler/bid_handler.h
+++ handler/bid_handler.h
@@ -6,21 +6,23 @@
 
 #include <vector>
 #include "../include/common.h"
-#include "../manager/ad_index.h"
-#include "../manager/campaign_index.h"
+#include "../manager/index_manager.h"
 
 class BidHandler {
  public:
   BidHandler() {};
-  void doBidding(const BidReqData& bidReqData, BidRspData& bidRspData);
+  void Init();
+  void Bid(const BidReqData& bidReqData, BidRspData& bidRspData);
  private:
-  bool targetingFilter(const BidReqData& bidReqData);
+  bool reqFilter(const BidReqData& bidReqData);
+  std::unordered_map<int32_t, Campaign> campaignFilter(const BidReqData& bidReqData);
+  std::unordered_map<int32_t, Ad> adFilter(const BidReqData& bidReqData, const std::unordered_map<int32_t, Campaign>& campaignMap);
   bool rank();
   std::vector<Ad> selectAds();
   BidRspData fillResponse(const BidReqData& bidReqData, std::vector<Ad> rspAdList);
  private:
-  AdIndex* ad_index_;
-  CampaignIndex* campaign_index_;
+  IndexManager* index_manager_;
+  log4cpp::Category* logger_;
 };
 
 #endif //BIDDING_MODEL_SERVER_HANDLER_BID_HANDLER_H_
