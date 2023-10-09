73c5660522edcddff29cd0b097e393c474156ab4
LPT <chut1987@gmail.com>
2023-10-09 22:00:48 +0800

    Add files via upload

--- service/bid_service.cpp
+++ service/bid_service.cpp
@@ -2,11 +2,26 @@
 //
 
 #include "bid_service.h"
+#include "../include/common.h"
+#include "../manager/index_manager.h"
 
-Status bidAd(ServerContext* context, const BidRequest* request, BidResponse* response) {
+//Status bidAd(ServerContext* context, const BidRequest* request, BidResponse* response) {
+//
+//  // 示例：简单地将请求的 id 复制到响应中
+//  response->set_id(request->id());
+//
+//  return Status::OK;
+//}
 
-  // 示例：简单地将请求的 id 复制到响应中
-  response->set_id(request->id());
+BidService::BidService() {
+  bid_handler_ = std::make_shared<BidHandler>();
+  bid_handler_->Init();
+}
 
-  return Status::OK;
+void BidService::bidAdTest() {
+
+  auto logger = getLogger();
+  logger->info("do bid test");
+
+  auto indexManager = IndexManager::getInstance();
 }
--- service/bid_service.h
+++ service/bid_service.h
@@ -5,27 +5,27 @@
 #define BIDDING_MODEL_SERVER_SERVICE_BID_SERVICE_H_
 
 #include <grpcpp/grpcpp.h>
-#include "../proto/ad_bid.pb.h"
-#include "../proto/ad_bid.grpc.pb.h"
+#include "../handler/bid_handler.h"
+//#include "../proto/ad_bid.pb.h"
+//#include "../proto/ad_bid.grpc.pb.h"
+//using grpc::Server;
+//using grpc::ServerBuilder;
+//using grpc::ServerContext;
+//using grpc::Status;
+//using ad::BidRequest;
+//using ad::BidResponse;
 
-
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
-
-//class GreeterServiceImpl final : public Greeter::Service {
-//  Status SayHello(ServerContext* context, const HelloRequest* request,
-//                  HelloReply* reply) override {
-//    std::string prefix("Hello, ");
-//    reply->set_message(prefix + request->name());
-//    return Status::OK;
-//  }
+//class BidService final : public ad::AdBidderService::Service {
+// public:
+//  Status bidAd(ServerContext* context, const BidRequest* request, BidResponse* response);
 //};
 
- class BidService final : public AdBidderService::Service {
+class BidService {
  public:
-  Status bidAd(ServerContext* context, const BidRequest* request, BidResponse* response);
+  BidService();
+  void bidAdTest();
+ private:
+  std::shared_ptr<BidHandler> bid_handler_;
 };
 
 #endif //BIDDING_MODEL_SERVER_SERVICE_BID_SERVICE_H_
